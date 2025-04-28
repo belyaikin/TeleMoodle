e4a0c2586886ccf93791ce93d0cf44e21b925b26
belyaikin <dmitriybelyaikin@gmail.com>
2025-04-28 13:57:13 +0500

    refactor, implement getting user courses

--- src/main/java/belyaikin/telemoodle/model/moodle/MoodleCourse.java
+++ src/main/java/belyaikin/telemoodle/model/moodle/MoodleCourse.java
@@ -3,7 +3,7 @@ package belyaikin.telemoodle.model.moodle;
 import lombok.Data;
 
 @Data
-public class Course {
+public class MoodleCourse {
+    private int id;
     private String name;
-    private String teacher;
 }
