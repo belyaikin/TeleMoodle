b8bb59a3d31320a097d5f76363ae85cb65cd47f2
belyaikin <dmitriybelyaikin@gmail.com>
2025-04-29 11:29:11 +0500

    delete unnecessary folders

--- backend/src/main/java/belyaikin/telemoodle/model/moodle/MoodleCourse.java
+++
@@ -1,9 +0,0 @@
-package belyaikin.telemoodle.model.moodle;
-
-import lombok.Data;
-
-@Data
-public class MoodleCourse {
-    private int id;
-    private String name;
-}
--- backend/src/main/java/belyaikin/telemoodle/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package belyaikin.telemoodle.repository;
-
-import belyaikin.telemoodle.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByTelegramId(long id);
-}
