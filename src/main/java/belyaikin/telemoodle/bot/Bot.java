b1af119eb39d4d17881980e9d02a1ea92f6e0c5b
belyaikin <dmitriybelyaikin@gmail.com>
2025-05-18 19:53:26 +0500

    make the deadlines message prettier

--- src/main/java/belyaikin/telemoodle/bot/Bot.java
+++ src/main/java/belyaikin/telemoodle/bot/Bot.java
@@ -272,24 +272,24 @@ public class Bot extends TelegramLongPollingBot {
         List<Deadline> deadlines = moodleService.getAllDeadlines(token);
 
         SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy, HH:mm");
-        formatter.setTimeZone(TimeZone.getTimeZone("Asia/Yekaterinburg"));
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT+5:00"));
 
         StringBuilder messageText = new StringBuilder();
-        messageText.append("Here are your upcoming deadlines:\n\n");
+        messageText.append("\uD83D\uDDD3 Here are your upcoming deadlines:\n\n");
 
         SimpleDateFormat sdf = new SimpleDateFormat("E MMM dd HH:mm:ss", Locale.ENGLISH);
 
         for (Deadline deadline : deadlines) {
             if (deadline.name().contains("Attendance")) continue;
+
             long timestampMillis = deadline.timeEnd() * 1000L;
             Date date = new Date(timestampMillis);
             String formattedDate = sdf.format(date);
+
             messageText
-                    .append("-----------------").append("\n")
-                    .append("Course: ").append(deadline.course().name()).append("  ||  ")
-                    .append(deadline.name()).append("  ||  ")
-                    .append("Due Date: ").append(formattedDate).append("  ||  ")
-                    .append("Is Last Day: ").append(deadline.lastDay() ? "Yes" : "No").append("\n");
+                    .append(deadline.lastDay() ? "❗⏰ " : "⏰ ")
+                    .append(deadline.name()).append(" (").append(deadline.course().name()).append(") ")
+                    .append("is until ").append(formattedDate).append("\n\n");
         }
 
         sendRegularMessage(chatId, messageText.toString());
