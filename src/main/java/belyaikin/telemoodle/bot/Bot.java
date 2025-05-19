32c0bde54ded7aa43168bc1342a5dd8bf3fcc50c
belyaikin <dmitriybelyaikin@gmail.com>
2025-05-19 17:32:43 +0500

    unused imports

--- src/main/java/belyaikin/telemoodle/bot/Bot.java
+++ src/main/java/belyaikin/telemoodle/bot/Bot.java
@@ -1,11 +1,6 @@
 package belyaikin.telemoodle.bot;
 
 import belyaikin.telemoodle.TeleMoodleApplication;
-import belyaikin.telemoodle.model.User;
-import belyaikin.telemoodle.model.moodle.*;
-import belyaikin.telemoodle.model.moodle.course.Deadline;
-import belyaikin.telemoodle.model.moodle.course.Grade;
-import belyaikin.telemoodle.model.moodle.course.Course;
 import belyaikin.telemoodle.service.MoodleService;
 import belyaikin.telemoodle.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 @Component
 public class Bot extends TelegramLongPollingBot {
